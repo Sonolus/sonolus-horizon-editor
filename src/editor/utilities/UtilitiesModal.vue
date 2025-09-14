afa73cb5b88434a61d036ab65936a148c4305f9e
Burrito <burritoyangster@gmail.com>
2025-09-14 14:34:30 +0800

    Added cover editor

---
+++ src/editor/utilities/UtilitiesModal.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { i18n } from '../../i18n'
+import { showModal } from '../../modals'
+import BaseModal from '../../modals/BaseModal.vue'
+import CoverEditorModal from './coverEditor/CoverEditorModal.vue'
+
+const utilities = {
+    coverEditor: CoverEditorModal,
+}
+</script>
+
+<template>
+    <BaseModal :title="i18n.utilities.title">
+        <button
+            v-for="(modal, name) in utilities"
+            :key="name"
+            class="bg-[#222] px-2 py-1 transition-colors hover:bg-[#444] active:bg-[#111]"
+            @click="showModal(modal, {})"
+        >
+            {{ i18n.utilities[name].title }}
+        </button>
+    </BaseModal>
+</template>
