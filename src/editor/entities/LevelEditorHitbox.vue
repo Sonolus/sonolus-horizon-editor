2c211489d4ace46fa8a64ff5aab49f74a3e034c4
Burrito <burritoyangster@gmail.com>
2025-09-17 14:20:10 +0800

    Added and refactored to use hitbox

---
+++ src/editor/entities/LevelEditorHitbox.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { bpms } from '../../history/bpms'
+import type { EntityHitbox } from '../../state/entities'
+import { beatToTime } from '../../state/integrals/bpms'
+import { viewBox } from '../view'
+
+defineProps<{
+    hitbox: EntityHitbox
+}>()
+</script>
+
+<template>
+    <rect
+        :x="3.5 - hitbox.lane - hitbox.w - 0.1"
+        :y="beatToTime(bpms, hitbox.beat) * viewBox.ups - hitbox.t - 0.1"
+        :width="hitbox.w * 2 + 0.2"
+        :height="hitbox.t + hitbox.b + 0.2"
+        stroke="#fff"
+        stroke-dasharray="6 4"
+    />
+</template>
