25e6046766ed1e381bb8baed12c55b006ada1b80
Burrito <burritoyangster@gmail.com>
2025-11-03 20:35:03 +0800

    Refactored auto save

---
+++ src/history/autoSave/index.ts
@@ -0,0 +1,52 @@
+import { watch } from 'vue'
+import { isStateDirty, resetState, state } from '..'
+import { parseChart } from '../../chart/parse'
+import { validateChart } from '../../chart/validate'
+import { i18n } from '../../i18n'
+import { parseLevelData } from '../../levelData/parse'
+import { serializeLevelData } from '../../levelData/serialize'
+import { showModal } from '../../modals'
+import LoadingModal from '../../modals/LoadingModal.vue'
+import { settings } from '../../settings'
+import { storageGet, storageRemove, storageSet } from '../../storage'
+import { timeout } from '../../utils/promise'
+
+export const useAutoSave = () => {
+    let id: number | undefined
+
+    watch(
+        () => settings.autoSave && state.value,
+        (state) => {
+            clearTimeout(id)
+
+            if (!state) {
+                storageRemove('autoSave.levelData')
+                return
+            }
+
+            if (!isStateDirty.value) return
+
+            id = setTimeout(() => {
+                storageSet('autoSave.levelData', serializeLevelData(state.bgm.offset, state.store))
+            }, settings.autoSaveDelay * 1000)
+        },
+    )
+
+    const data = storageGet('autoSave.levelData')
+    if (data) {
+        void showModal(LoadingModal, {
+            title: () => i18n.value.history.autoSave.title,
+            async *task() {
+                yield () => i18n.value.history.autoSave.importing
+                await timeout(50)
+
+                const levelData = parseLevelData(data)
+
+                const chart = parseChart(levelData.entities)
+                validateChart(chart)
+
+                resetState(chart, levelData.bgmOffset)
+            },
+        })
+    }
+}
--- src/history/index.ts
+++ src/history/index.ts
@@ -1,17 +1,9 @@
-import { computed, ref, shallowReactive, watch } from 'vue'
+import { computed, ref, shallowReactive } from 'vue'
 import type { Chart } from '../chart'
-import { parseChart } from '../chart/parse'
-import { validateChart } from '../chart/validate'
 import { i18n } from '../i18n'
-import { parseLevelData } from '../levelData/parse'
-import { serializeLevelData } from '../levelData/serialize'
 import { showModal } from '../modals'
 import ConfirmModal from '../modals/ConfirmModal.vue'
-import LoadingModal from '../modals/LoadingModal.vue'
-import { settings } from '../settings'
 import { createState, type State } from '../state'
-import { storageGet, storageRemove, storageSet } from '../storage'
-import { timeout } from '../utils/promise'
 import { cleanupWaveform } from '../waveform'
 
 const defaultChart: Chart = {
@@ -37,7 +29,7 @@ const defaultChart: Chart = {
 
 const index = ref(0)
 
-const isStateDirty = computed(() => index.value > 0)
+export const isStateDirty = computed(() => index.value > 0)
 
 const states = shallowReactive([
     {
@@ -102,44 +94,4 @@ export const resetState = (chart?: Chart, offset?: number) => {
     })
 
     cleanupWaveform()
-}
-
-export const useAutoSave = () => {
-    let id: number | undefined
-
-    watch(
-        () => settings.autoSave && state.value,
-        (state) => {
-            clearTimeout(id)
-
-            if (!state) {
-                storageRemove('autoSave.levelData')
-                return
-            }
-
-            if (!isStateDirty.value) return
-
-            id = setTimeout(() => {
-                storageSet('autoSave.levelData', serializeLevelData(state.bgm.offset, state.store))
-            }, settings.autoSaveDelay * 1000)
-        },
-    )
-
-    const data = storageGet('autoSave.levelData')
-    if (data) {
-        void showModal(LoadingModal, {
-            title: () => i18n.value.history.autoSave.title,
-            async *task() {
-                yield () => i18n.value.history.autoSave.importing
-                await timeout(50)
-
-                const levelData = parseLevelData(data)
-
-                const chart = parseChart(levelData.entities)
-                validateChart(chart)
-
-                resetState(chart, levelData.bgmOffset)
-            },
-        })
-    }
 }
