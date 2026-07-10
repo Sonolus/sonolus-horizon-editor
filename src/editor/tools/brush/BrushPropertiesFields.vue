7b3f71d21457f00908014f3f881b10ce15646c49
Burrito <burritoyangster@gmail.com>
2026-07-10 08:04:25 +0800

    Refactor brush properties (#211)

---
+++ src/editor/tools/brush/BrushPropertiesFields.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { brushProperties } from '.'
+import OptionalColorField from '../../../modals/form/OptionalColorField.vue'
+import OptionalEaseField from '../../../modals/form/OptionalEaseField.vue'
+import OptionalIgnoreTimeScaleField from '../../../modals/form/OptionalIgnoreTimeScaleField.vue'
+import OptionalScaleLField from '../../../modals/form/OptionalScaleLField.vue'
+import OptionalScaleRField from '../../../modals/form/OptionalScaleRField.vue'
+import { useProperties } from '../../utils/properties'
+
+const createModel = useProperties(brushProperties)
+
+const color = createModel('color')
+const scaleL = createModel('scaleL')
+const scaleR = createModel('scaleR')
+const ease = createModel('ease')
+const ignoreTimeScale = createModel('ignoreTimeScale')
+</script>
+
+<template>
+    <OptionalColorField v-model="color" />
+    <OptionalScaleLField v-model="scaleL" />
+    <OptionalScaleRField v-model="scaleR" />
+    <OptionalEaseField v-model="ease" />
+    <OptionalIgnoreTimeScaleField v-model="ignoreTimeScale" />
+</template>
--- src/editor/tools/brush/BrushSidebar.vue
+++ src/editor/tools/brush/BrushSidebar.vue
@@ -1,29 +1,11 @@
 <script setup lang="ts">
-import { brushProperties } from '.'
 import { i18n } from '../../../i18n'
-import OptionalColorField from '../../../modals/form/OptionalColorField.vue'
-import OptionalEaseField from '../../../modals/form/OptionalEaseField.vue'
-import OptionalIgnoreTimeScaleField from '../../../modals/form/OptionalIgnoreTimeScaleField.vue'
-import OptionalScaleLField from '../../../modals/form/OptionalScaleLField.vue'
-import OptionalScaleRField from '../../../modals/form/OptionalScaleRField.vue'
 import BaseSidebar from '../../sidebars/BaseSidebar.vue'
-import { useProperties } from '../../utils/properties'
-
-const createModel = useProperties(brushProperties)
-
-const color = createModel('color')
-const scaleL = createModel('scaleL')
-const scaleR = createModel('scaleR')
-const ease = createModel('ease')
-const ignoreTimeScale = createModel('ignoreTimeScale')
+import BrushPropertiesFields from './BrushPropertiesFields.vue'
 </script>
 
 <template>
     <BaseSidebar :title="i18n.tools.brush.sidebar.title">
-        <OptionalColorField v-model="color" />
-        <OptionalScaleLField v-model="scaleL" />
-        <OptionalScaleRField v-model="scaleR" />
-        <OptionalEaseField v-model="ease" />
-        <OptionalIgnoreTimeScaleField v-model="ignoreTimeScale" />
+        <BrushPropertiesFields />
     </BaseSidebar>
 </template>
