de594aa8e400f287e1cd81e4b9beb4a3f2875abc
NonSpicyBurrito <burritoyangster@gmail.com>
2026-07-10 08:01:48 +0800

    Refactored brush properties

--- src/editor/commands/brush/BrushPropertiesModal.vue
+++ src/editor/commands/brush/BrushPropertiesModal.vue
@@ -1,29 +1,11 @@
 <script setup lang="ts">
 import { i18n } from '../../../i18n'
-import OptionalColorField from '../../../modals/form/OptionalColorField.vue'
-import OptionalEaseField from '../../../modals/form/OptionalEaseField.vue'
-import OptionalIgnoreTimeScaleField from '../../../modals/form/OptionalIgnoreTimeScaleField.vue'
-import OptionalScaleLField from '../../../modals/form/OptionalScaleLField.vue'
-import OptionalScaleRField from '../../../modals/form/OptionalScaleRField.vue'
 import PropertiesModal from '../../../modals/form/PropertiesModal.vue'
-import { brushProperties } from '../../tools/brush'
-import { useProperties } from '../../utils/properties'
-
-const createModel = useProperties(brushProperties)
-
-const color = createModel('color')
-const scaleL = createModel('scaleL')
-const scaleR = createModel('scaleR')
-const ease = createModel('ease')
-const ignoreTimeScale = createModel('ignoreTimeScale')
+import BrushPropertiesFields from '../../tools/brush/BrushPropertiesFields.vue'
 </script>
 
 <template>
     <PropertiesModal :title="i18n.commands.brush.modal.title">
-        <OptionalColorField v-model="color" />
-        <OptionalScaleLField v-model="scaleL" />
-        <OptionalScaleRField v-model="scaleR" />
-        <OptionalEaseField v-model="ease" />
-        <OptionalIgnoreTimeScaleField v-model="ignoreTimeScale" />
+        <BrushPropertiesFields />
     </PropertiesModal>
 </template>
