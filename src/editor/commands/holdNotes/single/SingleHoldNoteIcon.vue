cd8ff2025c9bd997661b35c0b753c0bccd270000
Burrito <burritoyangster@gmail.com>
2026-07-28 05:21:28 +0800

    Improve hold note command icons (#224)

--- src/editor/commands/holdNotes/single/SingleHoldNoteIcon.vue
+++ src/editor/commands/holdNotes/single/SingleHoldNoteIcon.vue
@@ -5,11 +5,8 @@ import { colors } from '../../../../colors'
 <template>
     <svg viewBox="-0.55 -0.55 1.1 1.1">
         <rect x="-0.5" y="-0.5" width="1" height="1" :fill="colors[0]" fill-opacity="0.25" />
-        <rect
-            x="-0.5"
-            y="0.25"
-            width="1"
-            height="0.25"
+        <polygon
+            points="-0.5 0.5 0 0.15 0.5 0.5"
             stroke="#fff"
             stroke-width="0.05"
             :fill="colors[0]"
